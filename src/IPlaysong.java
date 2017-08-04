34bb94c3ff64bb426013d813f2476900c79aaa45
Ivan <ngk-trade@mail.ru>
2017-08-04 21:22:29 +0300

    Добавил абстрактные классы, изменил иерархию

---
+++ src/IPlaysong.java
@@ -0,0 +1,9 @@
+/**
+ * Created by 1 on 04.08.2017.
+ */
+public interface Playsong {
+    String song = "Не было печали";
+
+    void playSong();
+
+}
--- src/PleerArchSong.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by 1 on 04.08.2017.
- */
-public class PlayerArchSong {
-}
